f76088f975847ce1c245ee563f2e55436bb0015a
JJ Rogers <jjrogers619@gmail.com>
2017-01-25 20:13:25 -0500

    Add files via upload

---
+++ itsrath.php
@@ -0,0 +1,14 @@
+<?php
+  $servername = "localhost";
+  $username = "techjrog_its492";
+  $password = "Buster@619";
+  $dbname = "techjrog_rathskellar";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  ?>
+  <!-- end PHP script -->
